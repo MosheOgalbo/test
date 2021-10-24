38a2630e91591e943e7823ad74dd42b70a1bca23
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-24 23:04:35 +0300

    test

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import time
 from selenium import webdriver
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-import unittest
 
 def print_hi(name):
 
     driver = webdriver.Chrome("/Users/moshe_ogalbo/test/chromedriver")
     driver.maximize_window()
-    time.sleep(1)
-
-#    driver.implicitly_wait(5)
+    driver.implicitly_wait(1)
     driver.get('https://www.ynet.co.il/')
+    driver.refresh()
     print(driver.current_url)
-    driver.find_element()
-    driver.find_element
+    print(driver.title)
+
     #   wait = WebDriverWait(driver, 20)
     #wait.until(driver.find_element_by_id("main_header_weather").size())
    # search_box = driver.find_element_by_name("topStoryDate").text
