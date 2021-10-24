bee2e448069e5429679568ae5805c84f6b11040e
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-24 20:09:48 +0300

    test

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 
 def print_hi(name):
+
     driver = webdriver.Chrome("/Users/moshe_ogalbo/test/chromedriver")
     driver.maximize_window()
+    wait = WebDriverWait(driver, 50)
     driver.get('https://www.ynet.co.il/')
+    time.sleep(2)
+    wait.until(driver.find_element_by_link_text("logo").click())
 
-    time.sleep(5)
+    search_box = driver.find_element_by_name("topStoryDate")
 
-    search_box = driver.find_elements_by_class_name("textDiv").find_element_by_tag_name("topStoryDate")
-
-    print(search_box)
-
-
+    print(search_box.tag_name)
 
     time.sleep(5)
 
     driver.quit()
 
 if __name__ == '__main__':
-    print_hi('name')
+    print_hi('PyCharm')
